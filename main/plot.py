f6e0a7d73f1bd9568f2e0bf9dc71ff20bb8b50f5
Agrima Jain <116493142+ughrima@users.noreply.github.com>
2024-04-15 17:11:52 +0530

    Update plot.py

--- main/plot.py
+++ main/plot.py
@@ -1,31 +1,28 @@
+# To plot the graphs 
+
 import matplotlib.pyplot as plt
 
-# Sample data for plotting
+# Bar graph
 x = [1, 2, 3, 4, 5]
 y = [10, 20, 25, 15, 30]
 labels = ['E', 'F', 'G', 'H', 'I']
 
+plt.figure(figsize=(8, 6))
+plt.bar(x, y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Bar Graph')
+plt.xticks(x, labels)
+plt.show()
 
-# Bar graph
-# plt.figure(figsize=(8, 6))
-# plt.bar(x, y)
-# plt.xlabel('X-axis')
-# plt.ylabel('Y-axis')
-# plt.title('Bar Graph')
-# plt.xticks(x, labels)
-# plt.show()
-
-# Sample data for line graph
+# line graph
 p = [1, 2, 3, 4, 5]
 q = [2, 4, 6, 8, 10]
 
-# Plotting the line graph
 plt.plot(p, q)
 
-# Adding labels and title
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.title('Line Graph')
 
-# Display the plot
 plt.show()
